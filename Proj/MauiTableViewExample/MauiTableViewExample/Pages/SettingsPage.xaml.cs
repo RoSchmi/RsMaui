7c2ec9e50d206a04f7757e083487c64305070268
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2023-01-08 17:29:52 +0100

    MauiTableViewExample Clean up

--- Proj/MauiTableViewExample/MauiTableViewExample/Pages/SettingsPage.xaml.cs
+++ Proj/MauiTableViewExample/MauiTableViewExample/Pages/SettingsPage.xaml.cs
@@ -5,13 +5,11 @@ namespace MauiTableViewExample.Pages;
 public partial class SettingsPage : ContentPage
 
 {
-    //SettingsViewModel vm;
     SettingsViewModel vm = new SettingsViewModel();
-    //public SettingsPage(SettingsViewModel theViewModel)
+    
     public SettingsPage()
     {
-		InitializeComponent();
-        //vm = new SettingsViewModel();
+		InitializeComponent();      
         BindingContext = vm;
         TableViewAccounts.Root = vm.AccountsTableRoot;
     }
